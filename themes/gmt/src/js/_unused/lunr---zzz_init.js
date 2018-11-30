0e3de5405b81f8571acc7aa4a9bbee4e935c7519
cferdinandi <chris@gomakethings.com>
2018-11-30 16:32:34 -0500

    Updated

--- themes/gmt/src/js/_unused/lunr---zzz_init.js
+++ themes/gmt/src/js/_unused/lunr---zzz_init.js
@@ -11,7 +11,7 @@
 	var form = document.querySelector('#form-search');
 	var input = document.querySelector('#input-search');
 	var resultList = document.querySelector('#search-results');
-	var timeout, searchIndex, idx;
+	var timeout, idx;
 
 	// Only run if the form input and results container exist
 	if (!form || !input || !resultList) return;
@@ -20,6 +20,23 @@
 	//
 	// Methods
 	//
+
+	// Setup Lunr.js
+	var setupLunr = function () {
+		return lunr(function () {
+			var search = this;
+			search.ref('id');
+			search.field('title');
+			search.field('content');
+
+			if (searchIndex.length > 0) {
+				for (var i = 0; i < searchIndex.length; i++) {
+					searchIndex[i].id = i;
+					search.add(searchIndex[i]);
+				}
+			}
+		});
+	};
 
 	// Get the value of a query string from a URL
 	var getQueryString = function (field, url) {
@@ -62,54 +79,6 @@
 		displayResults(results);
 	};
 
-	// Create a search index in Lunr
-	var createIndex = function (searchIndex, query) {
-		idx = lunr(function () {
-			var search = this;
-			search.ref('id');
-			search.field('title');
-			search.field('content');
-
-			if (searchIndex.length > 0) {
-				for (var i = 0; i < searchIndex.length; i++) {
-					searchIndex[i].id = i;
-					search.add(searchIndex[i]);
-				}
-			}
-		});
-		runSearch(query)
-	};
-
-	// Setup Lunr.js
-	var setupLunr = function (query) {
-
-		// Set up our HTTP request
-		var xhr = new XMLHttpRequest();
-
-		// Setup our listener to process compeleted requests
-		xhr.onreadystatechange = function () {
-
-			// Only run if the request is complete
-			if (xhr.readyState !== 4) return;
-
-			// What do when the request is successful
-			if (xhr.status >= 200 && xhr.status < 300) {
-				searchIndex = JSON.parse(xhr.responseText);
-				createIndex(searchIndex, query);
-				return;
-			}
-
-			// What do when the request fails
-			displayResults([]);
-
-		};
-
-		// Create and send a GET request
-		xhr.open('GET', '/api/search.json');
-		xhr.send();
-
-	};
-
 
 	//
 	// Event Listeners & Inits
@@ -119,41 +88,33 @@
 	input.value = input.value.replace(' site:gomakethings.com', '');
 
 	// If there's a querystring on load, search for it
-	// var query = getQueryString('s');
-	// if (query) {
-	// 	input.value = decodeURI(query);
-	// 	resultList.innerHTML = 'Searching...';
-	// 	setTimeout(function () {
-	// 		if (!idx) {
-	// 			idx = setupLunr(query);
-	// 		}
-	// 		runSearch(decodeURI(query));
-	// 	}, 10);
-	// }
+	var query = getQueryString('s');
+	if (query) {
+		input.value = decodeURI(query);
+		resultList.innerHTML = 'Searching...';
+		setTimeout(function () {
+			if (!idx) {
+				idx = setupLunr();
+			}
+			runSearch(decodeURI(query));
+		}, 10);
+	}
 
 	// Prevent default submit event
 	form.addEventListener('submit', function (event) {
 		event.preventDefault();
 		resultList.innerHTML = 'Searching...';
 		setTimeout(function () {
-
-			// If there's no search value
+			if (!idx) {
+				idx = setupLunr();
+			}
 			if (input.value.length < 1) {
 				resultList.innerHTML = '';
 				updateURL('');
-				return;
+			} else {
+				runSearch(input.value);
+				updateURL(input.value);
 			}
-
-			// If there's no search index, create one
-			if (!idx) {
-				setupLunr();
-				return;
-			}
-
-			// Run the search
-			runSearch(input.value);
-			updateURL(input.value);
-
 		}, 10);
 	}, false);
 
@@ -161,6 +122,7 @@
 	if (history.pushState) {
 		window.addEventListener('popstate', function (event) {
 			query = getQueryString('s');
+			console.log(query);
 			if (query) {
 				runSearch(decodeURI(query));
 				input.value = decodeURI(query);
--- themes/gmt/static/js/lunr.js
+++ themes/gmt/static/js/lunr.js
@@ -2996,7 +2996,7 @@ lunr.QueryParser.parseBoost = function (parser) {
 	var form = document.querySelector('#form-search');
 	var input = document.querySelector('#input-search');
 	var resultList = document.querySelector('#search-results');
-	var timeout, idx;
+	var timeout, searchIndex, idx;
 
 	// Only run if the form input and results container exist
 	if (!form || !input || !resultList) return;
@@ -3005,23 +3005,6 @@ lunr.QueryParser.parseBoost = function (parser) {
 	//
 	// Methods
 	//
-
-	// Setup Lunr.js
-	var setupLunr = function () {
-		return lunr((function () {
-			var search = this;
-			search.ref('id');
-			search.field('title');
-			search.field('content');
-
-			if (searchIndex.length > 0) {
-				for (var i = 0; i < searchIndex.length; i++) {
-					searchIndex[i].id = i;
-					search.add(searchIndex[i]);
-				}
-			}
-		}));
-	};
 
 	// Get the value of a query string from a URL
 	var getQueryString = function (field, url) {
@@ -3064,6 +3047,54 @@ lunr.QueryParser.parseBoost = function (parser) {
 		displayResults(results);
 	};
 
+	// Create a search index in Lunr
+	var createIndex = function (searchIndex, query) {
+		idx = lunr((function () {
+			var search = this;
+			search.ref('id');
+			search.field('title');
+			search.field('content');
+
+			if (searchIndex.length > 0) {
+				for (var i = 0; i < searchIndex.length; i++) {
+					searchIndex[i].id = i;
+					search.add(searchIndex[i]);
+				}
+			}
+		}));
+		runSearch(query)
+	};
+
+	// Setup Lunr.js
+	var setupLunr = function (query) {
+
+		// Set up our HTTP request
+		var xhr = new XMLHttpRequest();
+
+		// Setup our listener to process compeleted requests
+		xhr.onreadystatechange = function () {
+
+			// Only run if the request is complete
+			if (xhr.readyState !== 4) return;
+
+			// What do when the request is successful
+			if (xhr.status >= 200 && xhr.status < 300) {
+				searchIndex = JSON.parse(xhr.responseText);
+				createIndex(searchIndex, query);
+				return;
+			}
+
+			// What do when the request fails
+			displayResults([]);
+
+		};
+
+		// Create and send a GET request
+		xhr.open('GET', '/api/search.json');
+		xhr.send();
+
+	};
+
 
 	//
 	// Event Listeners & Inits
@@ -3073,33 +3104,41 @@ lunr.QueryParser.parseBoost = function (parser) {
 	input.value = input.value.replace(' site:gomakethings.com', '');
 
 	// If there's a querystring on load, search for it
-	var query = getQueryString('s');
-	if (query) {
-		input.value = decodeURI(query);
-		resultList.innerHTML = 'Searching...';
-		setTimeout((function () {
-			if (!idx) {
-				idx = setupLunr();
-			}
-			runSearch(decodeURI(query));
-		}), 10);
-	}
+	// var query = getQueryString('s');
+	// if (query) {
+	// 	input.value = decodeURI(query);
+	// 	resultList.innerHTML = 'Searching...';
+	// 	setTimeout(function () {
+	// 		if (!idx) {
+	// 			idx = setupLunr(query);
+	// 		}
+	// 		runSearch(decodeURI(query));
+	// 	}, 10);
+	// }
 
 	// Prevent default submit event
 	form.addEventListener('submit', (function (event) {
 		event.preventDefault();
 		resultList.innerHTML = 'Searching...';
 		setTimeout((function () {
-			if (!idx) {
-				idx = setupLunr();
-			}
+
+			// If there's no search value
 			if (input.value.length < 1) {
 				resultList.innerHTML = '';
 				updateURL('');
-			} else {
-				runSearch(input.value);
-				updateURL(input.value);
+				return;
 			}
+
+			// If there's no search index, create one
+			if (!idx) {
+				setupLunr();
+				return;
+			}
+
+			// Run the search
+			runSearch(input.value);
+			updateURL(input.value);
+
 		}), 10);
 	}), false);
 
@@ -3107,7 +3146,6 @@ lunr.QueryParser.parseBoost = function (parser) {
 	if (history.pushState) {
 		window.addEventListener('popstate', (function (event) {
 			query = getQueryString('s');
-			console.log(query);
 			if (query) {
 				runSearch(decodeURI(query));
 				input.value = decodeURI(query);
