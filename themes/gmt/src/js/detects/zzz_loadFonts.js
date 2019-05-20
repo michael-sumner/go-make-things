4c0a39b02e00382a0cf789c1c087237a12c37d35
cferdinandi <chris@gomakethings.com>
2019-05-19 23:19:22 -0400

    Typography changes

--- themes/gmt/src/js/detects/zzz_loadFonts.js
+++ themes/gmt/src/js/detects/zzz_loadFonts.js
@@ -2,14 +2,15 @@
  * Load custom typeface
  */
 ;(function () {
-	var css = 'https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i';
+	var css = 'https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i|PT+Serif:400,400i,700';
 	if (getCookie('fontsLoaded')) {
 		loadCSS(css);
 		document.documentElement.className += ' fonts-loaded';
 	} else {
 		loadCSS(css);
-		var font = new FontFaceObserver('PT Serif');
-		font.load().then(function () {
+		var fontSans = new FontFaceObserver('PT Sans');
+		var fontSerif = new FontFaceObserver('PT Serif');
+		Promise.all([fontSans.load(), fontSerif.load()]).then(function () {
 			var expires = new Date(+new Date() + (7 * 24 * 60 * 60 * 1000)).toUTCString();
 			document.cookie = 'fontsLoaded=true; expires=' + expires;
 			document.documentElement.className += ' fonts-loaded';
