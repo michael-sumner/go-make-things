0f499df594323d7e87c9b865cfb6c1c90fd29567
cferdinandi <chris@gomakethings.com>
2019-10-06 12:44:57 -0400

    Updated font loading

--- themes/gmt/static/js/detects.js
+++ themes/gmt/static/js/detects.js
@@ -26,7 +26,12 @@ var getCookie = function (name) {
 		document.documentElement.className += ' fonts-loaded';
 		return;
 	}
-	document.fonts.load('italic bold 1em PT Serif').then((function () {
+	Promise.all([
+		document.fonts.load('1em PT Serif'),
+		document.fonts.load('italic 1em PT Serif'),
+		document.fonts.load('bold 1em PT Serif'),
+		document.fonts.load('italic bold 1em PT Serif')
+	]).then((function () {
 		var expires = new Date(+new Date() + (7 * 24 * 60 * 60 * 1000)).toUTCString();
 		document.cookie = 'fontsLoaded=true; expires=' + expires;
 		document.documentElement.className += ' fonts-loaded';
