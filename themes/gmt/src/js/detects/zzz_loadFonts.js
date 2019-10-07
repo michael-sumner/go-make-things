85d9c2232479eb987ae4a4b40625c4037699e893
cferdinandi <chris@gomakethings.com>
2019-10-06 22:49:49 -0400

    Attempting to fix FOIT

--- themes/gmt/src/js/detects/zzz_loadFonts.js
+++ themes/gmt/src/js/detects/zzz_loadFonts.js
@@ -1,14 +1,7 @@
 (function () {
 	if (!('fonts' in document)) return;
 	loadCSS('https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i');
-	Promise.all([
-		document.fonts.load('1em PT Serif'),
-		document.fonts.load('italic 1em PT Serif'),
-		document.fonts.load('bold 1em PT Serif'),
-		document.fonts.load('italic bold 1em PT Serif')
-	]).then(function () {
-		var expires = new Date(+new Date() + (7 * 24 * 60 * 60 * 1000)).toUTCString();
-		document.cookie = 'fontsLoaded=true; expires=' + expires;
+	document.fonts.load('1em PT Serif').then(function (res) {
 		document.documentElement.className += ' fonts-loaded';
 	});
 })();
