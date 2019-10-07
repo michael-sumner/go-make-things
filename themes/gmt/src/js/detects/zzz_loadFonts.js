93937a4ddc193dec721ab718aaf75853d6fbf003
cferdinandi <chris@gomakethings.com>
2019-10-06 23:00:41 -0400

    FOIT

--- themes/gmt/src/js/detects/zzz_loadFonts.js
+++
@@ -1,7 +0,0 @@
-(function () {
-	if (!('fonts' in document)) return;
-	loadCSS('https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i');
-	document.fonts.load('1em PT Serif').then(function (res) {
-		document.documentElement.className += ' fonts-loaded';
-	});
-})();
