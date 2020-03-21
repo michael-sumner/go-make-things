6481b003fe13291613433e128c4d6c3e7c4ae1d4
cferdinandi <chris@gomakethings.com>
2020-03-21 15:24:30 -0400

    Updated CTAs

---
+++ static/automate/rebuild.php
@@ -0,0 +1,34 @@
+<?php
+
+	/**
+	 * Automated deploy from GitHub
+	 *
+	 * https://developer.github.com/webhooks/
+	 * Template from ServerPilot (https://serverpilot.io/community/articles/how-to-automatically-deploy-a-git-repo-from-bitbucket.html)
+	 * Hash validation from Craig Blanchette (http://isometriks.com/verify-github-webhooks-with-php)
+	 */
+
+	// Variables
+	$secret = getenv('REFRESH_SECRET');
+	$key = $_GET['secret'];
+
+	// Validate key/secret
+	if (empty($secret) || empty($key) || $secret !== $key) {
+		die('Unable to complete this task at this time.');
+	}
+
+	// Rebuild all the sites
+	exec('cd /srv/users/serverpilot/apps/gomakethings/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/gomakethings/build/public/. /srv/users/serverpilot/apps/gomakethings/public');
+	exec('cd /srv/users/serverpilot/apps/vanillajspodcast/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/vanillajspodcast/build/public/. /srv/users/serverpilot/apps/vanillajspodcast/public');
+	exec('cd /srv/users/serverpilot/apps/gmtcourses/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/gmtcourses/build/public/. /srv/users/serverpilot/apps/gmtcourses/public');
+	exec('cd /srv/users/serverpilot/apps/vanillajsguides/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/vanillajsguides/build/public/. /srv/users/serverpilot/apps/vanillajsguides/public');
+	exec('cd /srv/users/serverpilot/apps/vanillajsacademy/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/vanillajsacademy/build/public/. /srv/users/serverpilot/apps/vanillajsacademy/public');
+	exec('cd /srv/users/serverpilot/apps/learnvanillajs/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/learnvanillajs/build/public/. /srv/users/serverpilot/apps/learnvanillajs/public');
+	exec('cd /srv/users/serverpilot/apps/vanillajslist/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/vanillajslist/build/public/. /srv/users/serverpilot/apps/vanillajslist/public');
+	exec('cd /srv/users/serverpilot/apps/vanillajstoolkit/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/vanillajstoolkit/build/public/. /srv/users/serverpilot/apps/vanillajstoolkit/public');
+	exec('cd /srv/users/serverpilot/apps/theleanweb/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/theleanweb/build/public/. /srv/users/serverpilot/apps/theleanweb/public');
+	exec('cd /srv/users/serverpilot/apps/vanillajsprojects/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/vanillajsprojects/build/public/. /srv/users/serverpilot/apps/vanillajsprojects/public');
+	exec('cd /srv/users/serverpilot/apps/vanillajsprepschool/build && /usr/local/bin/hugo && cp -r /srv/users/serverpilot/apps/vanillajsprepschool/build/public/. /srv/users/serverpilot/apps/vanillajsprepschool/public');
+
+	// Log the deployment
+	file_put_contents('rebuild.log', date('m/d/Y h:i:s a') . ' Rebuilt all the sites' . "\n", FILE_APPEND);
