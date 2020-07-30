e8b67bbb05eb416d6014b490d1f55b8f14b26a35
cferdinandi <chris@gomakethings.com>
2020-07-30 11:55:38 -0400

    Updated JS

--- themes/gmt/static/js/swInit.js
+++ themes/gmt/static/js/swInit.js
@@ -2,10 +2,12 @@
 (function () {
 	'use strict';
 
+	// Initialize the service worker
 	if (navigator && navigator.serviceWorker) {
 		navigator.serviceWorker.register('/sw.js');
 	}
 
+	// Cleanup old cache on page load
 	if (navigator.serviceWorker.controller) {
 		window.addEventListener('load', function () {
 			navigator.serviceWorker.controller.postMessage('cleanUp');
